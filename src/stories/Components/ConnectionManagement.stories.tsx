6afe7843a2b6b2166847d3b71d6c01afed981e65
Foxhoundn <filip.witosz@gmail.com>
2024-01-23 14:19:53 +0000

    Update test-storybook script in package.json and add testing-library imports

--- src/stories/Components/ConnectionManagement.stories.tsx
+++ src/stories/Components/ConnectionManagement.stories.tsx
@@ -4,6 +4,7 @@ import { ConnectionManagement } from '../../components/ConnectionManagement';
 import { AppsContext } from '../../context/apps';
 import apps from '../Data/apps.json';
 import { StoryMeta } from '../types';
+import { within, waitFor, fireEvent } from '@storybook/testing-library';
 
 const meta = {
   component: ConnectionManagement,
@@ -29,86 +30,96 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
-// export const Selected: Story = {
-//   args: {
-//     selectedConnection: 'google-calendar',
-//   },
-// };
+export const Selected: Story = {
+  args: {
+    selectedConnection: 'google-calendar',
+  },
+};
 
-// export const SelectedOAuth2UnAuthorized: Story = {
-//   name: 'Selected OAuth2 Un-Authorized',
-//   args: {
-//     selectedConnection: 'google-calendar',
-//     allowedValues: [
-//       {
-//         value: 'google-calendar',
-//         metadata: {
-//           needs_auth: true,
-//           oauth2_auth_code: true,
-//         },
-//       },
-//     ],
-//   },
-// };
+export const SelectedOAuth2UnAuthorized: Story = {
+  name: 'Selected OAuth2 Un-Authorized',
+  args: {
+    selectedConnection: 'google-calendar',
+    allowedValues: [
+      {
+        value: 'google-calendar',
+        metadata: {
+          needs_auth: true,
+          oauth2_auth_code: true,
+        },
+      },
+    ],
+  },
+};
 
-// export const SelectedOAuth2Authorized: Story = {
-//   name: 'Selected OAuth2 Authorized',
-//   args: {
-//     selectedConnection: 'google-calendar',
-//     allowedValues: [
-//       {
-//         value: 'google-calendar',
-//         metadata: {
-//           oauth2_auth_code: true,
-//         },
-//       },
-//     ],
-//   },
-// };
+export const SelectedOAuth2Authorized: Story = {
+  name: 'Selected OAuth2 Authorized',
+  args: {
+    selectedConnection: 'google-calendar',
+    allowedValues: [
+      {
+        value: 'google-calendar',
+        metadata: {
+          oauth2_auth_code: true,
+        },
+      },
+    ],
+  },
+};
 
-// export const NewConnection: Story = {
-//   args: {
-//     app: 'GoogleCalendar',
-//   },
-//   play: async ({ canvasElement }) => {
-//     const canvas = within(canvasElement);
+export const NewConnection: Story = {
+  args: {
+    app: 'GoogleCalendar',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
 
-//     await waitFor(() => canvas.getAllByText('Create new connection')[0], { timeout: 5000 });
+    await waitFor(() => canvas.getAllByText('Create new connection')[0], {
+      timeout: 5000,
+    });
 
-//     await fireEvent.click(canvas.getAllByText('Create new connection')[0]);
-//   },
-// };
+    await fireEvent.click(canvas.getAllByText('Create new connection')[0]);
+  },
+};
 
-// export const NewConnectionWithRequiredOptions: Story = {
-//   args: {
-//     app: 'Dynamics',
-//   },
-//   play: async ({ canvasElement, ...rest }) => {
-//     await NewConnection.play({ canvasElement, ...rest });
+export const NewConnectionWithRequiredOptions: Story = {
+  args: {
+    app: 'Dynamics',
+  },
+  play: async ({ canvasElement, ...rest }) => {
+    await NewConnection.play({ canvasElement, ...rest });
 
-//     await waitFor(
-//       () => expect(document.querySelectorAll('.reqore-collection-item').length).toBe(4),
-//       { timeout: 5000 }
-//     );
-//   },
-// };
+    await waitFor(
+      () =>
+        expect(
+          document.querySelectorAll('.reqore-collection-item').length
+        ).toBe(4),
+      { timeout: 5000 }
+    );
+  },
+};
 
-// export const EditingConnection: Story = {
-//   args: {
-//     app: 'GoogleCalendar',
-//   },
-//   play: async ({ canvasElement, ...rest }) => {
-//     const canvas = within(canvasElement);
+export const EditingConnection: Story = {
+  args: {
+    app: 'GoogleCalendar',
+  },
+  play: async ({ canvasElement, ...rest }) => {
+    const canvas = within(canvasElement);
 
-//     await NewConnection.play({ canvasElement, ...rest });
+    await NewConnection.play({ canvasElement, ...rest });
 
-//     await waitFor(() => canvas.getAllByText('Edit connection')[0], { timeout: 15000 });
+    await waitFor(() => canvas.getAllByText('Edit connection')[0], {
+      timeout: 15000,
+    });
 
-//     await fireEvent.click(canvas.getAllByText('Edit connection')[0]);
+    await fireEvent.click(canvas.getAllByText('Edit connection')[0]);
 
-//     await waitFor(
-//       () => expect(document.querySelectorAll('.reqore-collection-item').length).toBe(3),
-//       { timeout: 15000 }
-//     );
-//   },
-// };
+    await waitFor(
+      () =>
+        expect(
+          document.querySelectorAll('.reqore-collection-item').length
+        ).toBe(3),
+      { timeout: 15000 }
+    );
+  },
+};
