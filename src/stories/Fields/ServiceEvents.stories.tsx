8f914a9819053dbb7647a102b1319ef18681b1fd
Foxhoundn <filip.witosz@gmail.com>
2024-01-24 16:16:33 +0000

    Refactor ServiceEvents.stories.tsx

--- src/stories/Fields/ServiceEvents.stories.tsx
+++ src/stories/Fields/ServiceEvents.stories.tsx
@@ -1,7 +1,10 @@
 import { expect } from '@storybook/jest';
 import { Meta, StoryObj } from '@storybook/react';
 import { fireEvent, waitFor, within } from '@storybook/testing-library';
-import { IServiceEventList, ServiceEventListField } from '../../components/Field/serviceEvents';
+import {
+  IServiceEventList,
+  ServiceEventListField,
+} from '../../components/Field/serviceEvents';
 import serviceEvents from '../Data/serviceEvents.json';
 import { SwitchesToBuilder } from '../Tests/FSM/Basic.stories';
 import {
@@ -24,7 +27,9 @@ export const Existing: StoryObj<typeof meta> = {
   play: async () => {
     await waitFor(
       () => {
-        expect(document.querySelectorAll('.service-event-handler').length).toBe(3);
+        expect(document.querySelectorAll('.service-event-handler').length).toBe(
+          3
+        );
       },
       { timeout: 5000 }
     );
@@ -39,19 +44,30 @@ export const EventVariablesInFSM: StoryObj<typeof meta> = {
 
     await waitFor(
       () => {
-        expect(document.querySelectorAll('.service-event-handler').length).toBe(3);
+        expect(document.querySelectorAll('.service-event-handler').length).toBe(
+          3
+        );
       },
       { timeout: 5000 }
     );
 
-    await fireEvent.click(document.querySelectorAll('.select-reference-add-new')[0]);
+    await fireEvent.click(
+      document.querySelectorAll('.select-reference-add-new')[0]
+    );
 
     // @ts-expect-error
     await SwitchesToBuilder.play(playData);
 
+    await sleep(500);
+
     await _testsAddNewState('trigger', canvas);
 
+    await sleep(500);
+
     await _testsOpenAppCatalogue();
+
+    await sleep(500);
+
     await _testsSelectAppOrAction(canvas, 'Variables');
 
     await sleep(500);
