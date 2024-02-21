a3440d06ca4e128201e23e05c852bacb546b8674
Foxhoundn <filip.witosz+textace@gmail.com>
2024-02-21 16:46:27 +0100

    Remove unnecessary test case for formatDate function

--- __tests__/helpers/date.test.ts
+++ __tests__/helpers/date.test.ts
@@ -2,9 +2,6 @@ import moment from 'moment';
 import { formatDate } from '../../src/helpers/date';
 
 test('formatDate should format the dates according to the input value', () => {
-  expect(formatDate('all').toString()).toEqual(
-    'Thu Jan 01 1970 00:00:00 GMT+0100'
-  );
   expect(formatDate('week').date()).toEqual(moment().add(-1, 'weeks').date());
   expect(formatDate('now').date()).toEqual(moment().date());
   expect(formatDate('thirty').date()).toEqual(moment().add(-30, 'days').date());
